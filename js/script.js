e1aa80e798881b6743c044323303b2b4efdc1242
punikko <i.trust.shine.74@gmail.com>
2026-07-22 12:12:03 +0900

    最新版アップロード_20260722
    BBB英会話スクール（LP）

--- js/script.js
+++ js/script.js
@@ -12,3 +12,28 @@ openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   menuOverlay.classList.remove('is-open');
 });
+
+
+// BBBが選ばれる理由コンテンツのアニメーション
+$(function() {
+  $('.reason-list-item').on('inview', function(event, isInView) {
+    if (isInView) {
+      $(this).addClass('is-show');
+      // 1回だけアニメーションさせる場合
+      $(this).off('inview');
+    }
+  });
+});
+
+// 受講生の声コンテンツのアニメーション
+$(function() {
+  // .voice-list-item が画面内に表示されたときに処理を実行
+  $('.voice-list-item').on('inview', function(event, isInView) {
+    if (isInView) {
+      // 画面内に入ったら .is-show を追加
+      $(this).addClass('is-show');
+      // イベントを解除（1回だけ発火）
+      $(this).off('inview');
+    }
+  });
+});
